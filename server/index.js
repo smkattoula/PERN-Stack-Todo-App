e39f44cfb2a4f2fd86b50245d5e714141d08ecab
Shaker Kattoula <smkattoula@gmail.com>
2021-02-13 00:46:47 -0800

    connect server to the database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const cors = require("cors");
+const pool = require("./db");
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Routes //
+
+// Create a todo
+
+// Get all todos
+
+// Get a todo
+
+// Update a todo
+
+// Delete a todo
 
 app.listen(500, () => {
   console.log("Server has started on port: 5000");
