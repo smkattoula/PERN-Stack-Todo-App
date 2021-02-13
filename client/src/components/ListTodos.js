87ae4cf06d3b17c34b7ebd29a5f69288db22ca8e
Shaker Kattoula <smkattoula@gmail.com>
2021-02-13 03:26:42 -0800

    create ListTodos component, fetch all todos from database, create table to display todos along with todo id, description, edit and delete

---
+++ client/src/components/ListTodos.js
@@ -0,0 +1,48 @@
+import React, { Fragment, useState, useEffect } from "react";
+
+const ListTodos = () => {
+  const [todos, setTodos] = useState([]);
+
+  const getTodos = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/todos");
+      const data = await response.json();
+
+      setTodos(data);
+      console.log(data);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  return (
+    <Fragment>
+      <table className="table mt-5 text-center">
+        <thead>
+          <tr>
+            <th>Todo Id</th>
+            <th scope="col">Description</th>
+            <th scope="col">Edit</th>
+            <th scope="col">Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((todo, index) => (
+            <tr key={index}>
+              <td>{todo.todo_id}</td>
+              <td>{todo.description}</td>
+              <td>Edit</td>
+              <td>Delete</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Fragment>
+  );
+};
+
+export default ListTodos;
